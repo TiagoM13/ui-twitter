527a8d2009d654c7b6f28cc812fa27e1ee055e29
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-07 12:21:45 -0300

    feat: add twitter UI layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,76 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+
+import './global.css'
+
+import LogoTwitter from './assets/logo-twitter.svg'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="layout">
+      <aside className="sidebar">
+        <img src={LogoTwitter} alt="Logo Twitter" className="logo" />
+
+        <nav className="main-navigation">
+          <a href="" className="active">
+            <House weight='fill' />
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button type="button" className="new-tweet">
+          Tweet
+        </button>
+      </aside>
+
+      <div className="content">
+        <main className='timeline'>
+          <div className='timeline-header'>
+            Home
+            <Sparkle />
+          </div>
+
+          <form className='new-tweet-form'>
+            <label htmlFor="tweet">
+              <img src="https://github.com/TiagoM13.png" alt="Tiago Mota" />
+              <textarea id="tweet" placeholder="What's happening?" />
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+
+          <div className="separator" />
+        </main>
+      </div>
+    </div>
   </React.StrictMode>,
 )
